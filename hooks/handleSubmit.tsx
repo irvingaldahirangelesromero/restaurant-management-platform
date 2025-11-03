753e4ef46e66cda0b156e194c786a5b8248ec173
Irving Aldahir <irving.angelesromero@gmail.com>
2025-11-03 12:12:53 -0600

    handling the submit action

---
+++ hooks/handleSubmit.tsx
@@ -0,0 +1,51 @@
+import store from './localstore';
+import { useRedirect } from './useRedirect';
+
+
+export function useHandleSubmit() {
+
+    const { redirectTo } = useRedirect()
+
+    async function handleSubmit(
+        e: React.FormEvent,
+        url: string,
+        items: any,
+        setError: (v: string | null) => void,
+        nexturl?: string
+    ) {
+        e.preventDefault();
+        setError(null)
+
+        try {
+            const response = await fetch(
+                url,
+                {
+                    method: 'POST',
+                    headers: {
+                        'Content- Type': 'application / json'
+                    },
+                    body: JSON.stringify(items)
+                }
+            )
+
+            if (!response.ok) {
+                const errorData = await response.json();
+                throw new Error(errorData.message);
+            }
+            const data = await response.json();
+
+            store(
+                [
+                    ['token', data.token],
+                    ['user', data.user]
+                ]
+            )
+
+            if (nexturl) redirectTo(nexturl)
+        } catch (err: any) {
+            setError(err.message)
+        }
+    }
+
+    return { handleSubmit }
+}
