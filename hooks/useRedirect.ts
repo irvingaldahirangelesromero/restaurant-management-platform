6f456d497e88db1897137cef4491ff581b04900b
Irving Aldahir <irving.angelesromero@gmail.com>
2025-11-03 12:07:15 -0600

    component update

--- hooks/useRedirect.ts
+++ hooks/useRedirect.ts
@@ -5,7 +5,7 @@ import { useRouter } from "next/navigation";
 export function useRedirect() {
     const router = useRouter();
 
-    const redirectTo = (url: string) => {
+  const redirectTo = (url: string) => {
       router.push(url);
     };
     
