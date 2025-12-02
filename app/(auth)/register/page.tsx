5dc9dfe54673143338682fc8cd837ea012f0ba12
Irving Aldahir <irving.angelesromero@gmail.com>
2025-12-02 01:26:50 -0600

    form inputs sanitized

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,9 +1,12 @@
 'use client';
+
 import { useState } from 'react';
 import Button from '@/components/Button';
 import { useHandleSubmit } from '@/hooks/handleSubmit';
 import { registerWithEmail } from "@/actions/(email-actions)/sendEmailVerification";
 import { useRouter } from 'next/navigation';
+import { nameRegex, phoneRegex, emailRegex } from "@/utils/validators";
+import { length, lowercase, uppercase,number, specialChar} from "@/utils/validators";
 
 export default function RegisterPage() {
     const [nombre, setNombre] = useState('');
@@ -18,21 +21,26 @@ export default function RegisterPage() {
     const { handleSubmit } = useHandleSubmit();
     const router = useRouter();
 
-    const nameRegex = /^[A-Za-zÁÉÍÓÚáéíóúÑñ\s]{1,30}$/;
-    const phoneRegex = /^\d{10,15}$/;
-    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    const CheckIcon = () => (
+        <svg viewBox="0 0 24 24" className="w-3.5 h-3.5">
+            <path d="M0 0h24v24H0z" fill="none"></path>
+            <path fill="currentColor" d="M10 15.172l9.192-9.193 1.415 1.414L10 18l-6.364-6.364 1.414-1.414z"></path>
+        </svg>
+    );
 
-    const isNameValid = nameRegex.test(nombre);
-    const isLastnameValid = nameRegex.test(apellido);
-    const isPhoneValid = phoneRegex.test(telefono);
+    const XIcon = () => (
+        <svg viewBox="0 0 24 24" className="w-3.5 h-3.5">
+            <path fill="currentColor" d="M18.3 5.71L12 12l6.29 6.29-1.41 1.41L12 13.41l-6.29 6.29-1.41-1.41L10.59 12 4.29 5.71 5.7 4.29 12 10.59l6.29-6.3z" />
+        </svg>
+    );
 
     const getPasswordScore = (pwd: string) => {
         let score = 0;
-        if (/.{8,}/.test(pwd)) score++;
-        if (/[a-z]/.test(pwd)) score++;
-        if (/[A-Z]/.test(pwd)) score++;
-        if (/\d/.test(pwd)) score++;
-        if (/[!@#$%^&*(),.?":{}|<>_\-]/.test(pwd)) score++;
+        if (length.test(pwd)) score++;
+        if (lowercase.test(pwd)) score++;
+        if (uppercase.test(pwd)) score++;
+        if (number.test(pwd)) score++;
+        if (specialChar.test(pwd)) score++;
         return score;
     };
 
@@ -46,6 +54,23 @@ export default function RegisterPage() {
         "bg-green-900"
     ][Math.min(passwordScore, 4)];
 
+    const validations = {
+        length: length.test(password),
+        lowercase: lowercase.test(password),
+        uppercase: uppercase.test(password),
+        number: number.test(password),
+        special: specialChar.test(password)
+    }
+
+    const isNameValid = nameRegex.test(nombre);
+    const isLastnameValid = nameRegex.test(apellido);
+    const isPhoneValid = phoneRegex.test(telefono);
+    const isEmailValid = emailRegex.test(correo);
+    const isPasswordValid = Object.values(validations).every(Boolean);
+
+    const isFormFieldsValid = isNameValid && isLastnameValid && isPhoneValid && isEmailValid && isPasswordValid;
+
+    const isFormReady = isFormFieldsValid && agreed;
 
     return (
         <div className="min-h-screen bg-gray-50 lg:grid lg:grid-cols-[1fr_1.3fr] lg:items-center">
@@ -76,6 +101,20 @@ export default function RegisterPage() {
                     <form
                         className="space-y-4"
                         onSubmit={async (e) => {
+                            e.preventDefault();
+                           
+                            // validación final por seguridad (cliente)
+                            if (!isFormFieldsValid) {
+                                setError("Completa todos los campos correctamente antes de continuar.");
+                                return;
+                            }
+                            if (!agreed) {
+                                setError("Debes aceptar los términos y condiciones.");
+                                return;
+                            }
+
+                            setError(null);
+
                             const result = await handleSubmit(
                                 e,
                                 "/api/auth/register",
@@ -159,7 +198,7 @@ export default function RegisterPage() {
                             <span className="absolute left-4 top-0 -translate-y-1/2 bg-white px-1 text-xs text-gray-600">
                                 Correo electrónico*
                             </span>
-                            {correo && !emailRegex.test(correo) && (
+                            {correo && !isEmailValid && (
                                 <p className="text-xs text-red-500 mt-1">Ingresa un correo electrónico válido.</p>
                             )}
                         </div>
@@ -203,8 +242,8 @@ export default function RegisterPage() {
                                 name="password"
                                 type="password"
                                 required
-                                placeholder="password"
-                                autoComplete="new-password"
+                                placeholder="crea una contraseña"
+                                autoComplete="password"
                                 className="block w-full rounded-lg border border-gray-200 py-2.5 sm:py-2.5 px-3 sm:px-4 text-gray-900 shadow-sm placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-300 sm:text-sm sm:leading-6"
                                 value={password}
                                 onChange={(e) => setpassword(e.target.value)}
@@ -220,6 +259,61 @@ export default function RegisterPage() {
                                     ></div>
                                 </div>
                             )}
+                            <div>
+                                <p className="mt-5 text-sm font-medium text-gray-600 mb-3">
+                                    La Contraseña debe contener al menos:
+                                </p>
+
+                                <ul className="flex flex-col space-y-3 mt-3 text-sm">
+
+                                    <li className="flex items-center gap-2">
+                                        <span className={`${validations.length ? "bg-green-600" : "bg-gray-300"} text-white w-5 h-5 rounded-full flex items-center justify-center`}>
+                                            {validations.length ? <CheckIcon /> : <XIcon />}
+                                        </span>
+                                        <span className={validations.length ? "text-gray-800 font-medium" : "text-gray-500"}>
+                                            8 caracteres
+                                        </span>
+                                    </li>
+
+                                    <li className="flex items-center gap-2">
+                                        <span className={`${validations.uppercase ? "bg-green-600" : "bg-gray-300"} text-white w-5 h-5 rounded-full flex items-center justify-center`}>
+                                            {validations.uppercase ? <CheckIcon /> : <XIcon />}
+                                        </span>
+                                        <span className={validations.uppercase ? "text-gray-800 font-medium" : "text-gray-500"}>
+                                            Una letra mayúscula
+                                        </span>
+                                    </li>
+
+                                    <li className="flex items-center gap-2">
+                                        <span className={`${validations.lowercase ? "bg-green-600" : "bg-gray-300"} text-white w-5 h-5 rounded-full flex items-center justify-center`}>
+                                            {validations.lowercase ? <CheckIcon /> : <XIcon />}
+                                        </span>
+                                        <span className={validations.lowercase ? "text-gray-800 font-medium" : "text-gray-500"}>
+                                            Una letra minúscula
+                                        </span>
+                                    </li>
+
+                                    <li className="flex items-center gap-2">
+                                        <span className={`${validations.number ? "bg-green-600" : "bg-gray-300"} text-white w-5 h-5 rounded-full flex items-center justify-center`}>
+                                            {validations.number ? <CheckIcon /> : <XIcon />}
+                                        </span>
+                                        <span className={validations.number ? "text-gray-800 font-medium" : "text-gray-500"}>
+                                            Un número
+                                        </span>
+                                    </li>
+
+                                    <li className="flex items-center gap-2">
+                                        <span className={`${validations.special ? "bg-green-600" : "bg-gray-300"} text-white w-5 h-5 rounded-full flex items-center justify-center`}>
+                                            {validations.special ? <CheckIcon /> : <XIcon />}
+                                        </span>
+                                        <span className={validations.special ? "text-gray-800 font-medium" : "text-gray-500"}>
+                                            Un caracter especial (! @ # $ % & , . _ -)
+                                        </span>
+                                    </li>
+
+                                </ul>
+
+                            </div>
                         </div>
 
                         <div className="flex items-start pt-1 pb-3">
@@ -248,10 +342,11 @@ export default function RegisterPage() {
 
                         <button
                             type="submit"
-                            disabled={!agreed}
+                            disabled={!isFormReady}
+                            aria-disabled={!isFormReady}
                             className={`flex w-full justify-center rounded-xl px-3 py-2.5 text-sm font-semibold leading-6 text-white shadow-lg 
                                 transition-all duration-300 
-                                ${agreed ? "bg-[#232f38] hover:bg-[#3b4b57]" : "bg-[#232f38] opacity-40 cursor-not-allowed"}`}
+                                ${isFormReady ? "bg-[#232f38] hover:bg-[#3b4b57]" : "bg-[#232f38] opacity-40 cursor-not-allowed"}`}
                         >
                             Crear cuenta
                         </button>
