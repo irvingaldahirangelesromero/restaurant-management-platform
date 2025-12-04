601736801f05dbd6e2e6ea12cc609c4229c30364
Irving Aldahir <irving.angelesromero@gmail.com>
2025-12-04 11:56:02 -0600

    feat: Handling failed login attempts and password recovery

--- drizzle/0000_slippery_lila_cheney.sql
+++ drizzle/0000_slippery_lila_cheney.sql
@@ -7,5 +7,10 @@ CREATE TABLE "users" (
 	"password" text NOT NULL,
 	"role" varchar(20) DEFAULT 'user' NOT NULL,
 	"verified" boolean DEFAULT false NOT NULL,
+	"login_attempts" integer DEFAULT 0 NOT NULL,
+	"login_lock_until" bigint DEFAULT 0 NOT NULL,
+	"recovery_attempts" integer DEFAULT 0 NOT NULL,
+	"recovery_lock_until" bigint DEFAULT 0 NOT NULL,
+	"session time" bigint DEFAULT 0 NOT NULL,
 	CONSTRAINT "users_email_unique" UNIQUE("email")
 );
--- lib/schema.ts
+++ lib/schema.ts
@@ -1,4 +1,12 @@
-import { pgTable, serial, varchar, text, boolean } from "drizzle-orm/pg-core";
+import {
+  pgTable,
+  serial,
+  varchar,
+  text,
+  boolean,
+  bigint,
+  integer,
+} from "drizzle-orm/pg-core";
 
 export const users = pgTable("users", {
   id: serial("id").primaryKey(), // serial crea números autoincrementales
@@ -7,6 +15,11 @@ export const users = pgTable("users", {
   email: varchar("email", { length: 100 }).notNull().unique(),
   phone: varchar("phone", { length: 20 }).notNull(),
   password: text("password").notNull(),
-  role: varchar("role", { length: 20 }).default('user').notNull(),
+  role: varchar("role", { length: 20 }).default("user").notNull(),
   isVerified: boolean("verified").default(false).notNull(),
+  loginAttempts: integer("login_attempts").default(0).notNull(),
+  loginLockUntil: bigint("login_lock_until", { mode: "number" }).default(0).notNull(), //ms
+  recoveryAttempts: integer("recovery_attempts").default(0).notNull(),
+  recoveryLockUntil: bigint("recovery_lock_until", { mode: "number" }).default(0).notNull(),
+  timeSession: bigint("session time", { mode: "number" }).default(0).notNull(), //ms
 });
