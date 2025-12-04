f1e3bb295e18f137d35844c95ebb474ab6020ca6
Irving Aldahir <irving.angelesromero@gmail.com>
2025-12-04 11:56:02 -0600

    feat: Handling failed login attempts and password recovery

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -6,7 +6,6 @@ import { auth } from "@/lib/firebaseConfig";
 import { signInWithEmailAndPassword } from "firebase/auth";
 
 export async function POST(req: Request) {
-
   const body = await req.json();
   const { correo, password } = body;
 
@@ -27,7 +26,11 @@ export async function POST(req: Request) {
 
     if (!firebaseUser.emailVerified) {
       return Response.json(
-        { message: "Cuenta no verificada, revisa la bandeja de entrada de tu correo electronico, se ha enviado un correo de verificación", code: "UNVERIFIED" },
+        {
+          message:
+            "Cuenta no verificada, revisa la bandeja de entrada de tu correo electronico, se ha enviado un correo de verificación",
+          code: "UNVERIFIED",
+        },
         { status: 403 }
       );
     }
@@ -53,7 +56,18 @@ export async function POST(req: Request) {
   if (!usr)
     return Response.json({ message: "El usuario no existe" }, { status: 404 });
 
-  let ok 
+  const now = Date.now();
+  if (usr.loginLockUntil > now) {
+    const waitSeconds = Math.ceil((usr.loginLockUntil - now) / 1000);
+    return Response.json(
+      {
+        message: `Has superado el límite de intentos. Espera ${waitSeconds} segundos para volver a intentarlo.`,
+      },
+      { status: 429 }
+    );
+  }
+
+  let ok;
   try {
     ok = await compare(password, usr.password);
   } catch (bcryptError: any) {
@@ -65,6 +79,25 @@ export async function POST(req: Request) {
   }
 
   if (!ok) {
+    const newAttempts = usr.loginAttempts + 1;
+
+    if (newAttempts >= 3) {
+      const lockTimeMs = 30_000; // 30 segundos
+      await db
+        .update(users)
+        .set({ loginAttempts: 0, loginLockUntil: now + lockTimeMs })
+        .where(eq(users.id, usr.id));
+      return Response.json(
+        { message: "Demasiados intentos fallidos. Debes esperar 30 segundos." },
+        { status: 429 }
+      );
+    }
+    // Si aun no se llega al límite de intentos
+    await db
+      .update(users)
+      .set({ loginAttempts: newAttempts })
+      .where(eq(users.id, usr.id));
+
     return Response.json({ message: "Contraseña incorrecta" }, { status: 401 });
   }
 
@@ -72,13 +105,13 @@ export async function POST(req: Request) {
     // 1. Separa la contraseña del resto de los datos del usuario
     // 'password: _' significa: toma la contraseña y ponla en una variable '_' (que ignoraremos)
     // '...userWithoutPass' significa: pon el resto (nombre, email, ROLE) en esta variable
+    await db
+      .update(users)
+      .set({ loginAttempts: 0, loginLockUntil: 0 })
+      .where(eq(users.id, usr.id));
     const { password: _, ...userWithoutPass } = usr;
-
     // 2. Devuelve el usuario limpio al frontend
-    return Response.json({
-      ok: true,
-      user: userWithoutPass,
-    });
+    return Response.json({ ok: true, user: userWithoutPass });
   } catch (unexpectedError: any) {
     console.error("Error inesperado:", unexpectedError);
     return Response.json(
