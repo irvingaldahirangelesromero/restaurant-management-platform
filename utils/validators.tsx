638e702cdb44fd940722e71138b6e5e80874ece9
Irving Aldahir <irving.angelesromero@gmail.com>
2025-12-06 01:51:37 -0600

    Strengthening the regular expression for people's names

--- utils/validators.tsx
+++ utils/validators.tsx
@@ -1,6 +1,12 @@
-export const nameRegex = /^[A-Za-z\s]{1,30}$/;
+//another regex
 export const phoneRegex = /^\d{10,15}$/;
 export const emailRegex = /^(?!.*[._-]{2})[a-zA-Z0-9_][a-zA-Z0-9.-_]*[a-zA-Z0-9]+@[a-zA-Z0-9.]+\.[a-zA-Z]{2,3}$/;
+
+// name regex
+export const letters = /^[A-Za-záéíóúüñÁÉÍÓÚÜÑ]{1,30}$/;
+export const haveAnVowel = /[aeiouáéíóú]/i;
+export const noRepeatMoreThreeTimes = /(.)\1{2,}/;
+export const noRepeatConsonantsMoreThreeTimes = /(.)\1{2,}/;
 
 // password regex
 export const length = /.{8,}/;
