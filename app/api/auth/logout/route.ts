4f96fdf9f6721c464f5034f82109a06475e3a68a
Irving Aldahir <irving.angelesromero@gmail.com>
2026-02-11 16:17:45 -0600

    front

--- app/api/auth/logout/route.ts
+++
@@ -1,9 +0,0 @@
-import { deleteSession } from "@/lib/session";
-import { NextResponse } from "next/server";
-
-export async function POST() {
-    // Esta función borra la cookie 'session' del navegador
-    await deleteSession();
-    
-    return NextResponse.json({ ok: true });
-}
